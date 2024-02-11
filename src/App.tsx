875c802cb8f6281da0869bfc24fd953895a96c9c
Jeniffer Flores <hachikofu03@gmail.com>
2024-02-11 21:25:29 +0100

    Update styles and add classnames for the hamburger menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
 import React from "react";
 import classes from "./App.module.scss";
-import HamburgerMenu from "./components/HamburguerMenu/HamburguerMenu";
 import Button from "./components/Button.jsx/Button";
 import RRSSLogos from "./components/RRSSIcons/RRSSIcons";
+import Navbar from "./components/Navbar/Navbar";
 // import BackgroundImage from "./components/BackgroundImage";
 
-const sectionshb: {
-  name: string;
-  route: string;
-}[] = [
-  { name: "Logo", route: "none" },
-  { name: "Mensajes", route: "none" },
-  { name: "Proyectos", route: "none" },
-  { name: "Sobre mí", route: "none" },
-  { name: "CV", route: "none" },
-  { name: "Contacto", route: "none" },
-];
 
 const App: React.FC = () => {
   return (
       <div className={classes.base}>
-        <div className={classes.menu_base}>
-          <HamburgerMenu sections={sectionshb} />
-        </div>
-        <div>
+        <Navbar/>
+        {/* <div>
           <Button onClick={() => {}} text="Download CV" />
           <Button onClick={() => {}} text="Contact" />
         </div>
         <div>
           <RRSSLogos />
-        </div>
+        </div> */}
       </div>
   );
 };
